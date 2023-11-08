ba31dd3662a257e35c76b4cc1375ed8b19481edb
Arvin <arvinazad20@gmail.com>
2023-11-08 17:28:00 -0500

    This was pain

--- js/adventure.js
+++ js/adventure.js
@@ -1,23 +1,2 @@
-const adventureTextVar = document.querySelector(".adventureText");
-const changeText = (text) => {
-    adventureTextVar.innerText = text;
-}
-
-function buttonOne() {
-    var txt = "You encounter a blood thirsty vampire and die!<br> The End!";
-    document.getElementById("button-one").innerHTML = txt;
-
-
-}
-
-function changeText(text){
-    adventureTextVar.innerText = text;
-}
-
-function buttonTwo() {
-    var txt = "You escaped to live another day...<br> The End?";
-    document.getElementById("button-two").innerHTML = txt;
-}
-
-button1.addEventListener("click", changeText("Insert text here"));
-button2.addEventListener("click", changeText("Change text here"));
+let button1 = document.getElementsByClassName("button-option1");
+console.log(button1);
