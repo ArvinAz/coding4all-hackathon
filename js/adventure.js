2a33b24dd504fd06eb1e43bfa382b878af8c1a9c
Arvin <arvinazad20@gmail.com>
2023-11-08 21:11:55 -0500

    Added Functionality to the buttons

--- js/adventure.js
+++ js/adventure.js
@@ -1,2 +1,20 @@
-let button1 = document.getElementsByClassName("button-option1");
-console.log(button1);
+let buttons = document.querySelectorAll("button");
+let adventText = document.querySelector(".result");
+
+let button = document.querySelector(".button");
+console.log(button);
+
+buttons.forEach(function(item){
+    item.addEventListener("click", function(e){
+        console.log(e.target.classList);
+        x = e.target.classList;
+        if(x.contains("option1")){
+            adventText.textContent = 'You encounter a Blood Thirsty Vampire and Die! \n THe End';
+        }
+        else{
+            adventText.textContent = "You escaped to live for another day. The End!";
+        }
+
+    })
+
+})
